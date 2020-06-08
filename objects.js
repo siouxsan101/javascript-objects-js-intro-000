a0b5a351be6fd4d43de12500d4c95053e78592f4
susan hofmann <susanhofmannnyc@icloud.com>
2020-06-08 22:41:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -2,5 +2,8 @@ var playlist = {artist:"song"}
 
 function updatePlaylist(playlist,artistName,songTitle){
   playlist[artistName] = songTitle;
-  
+}
+
+removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName;
 }
